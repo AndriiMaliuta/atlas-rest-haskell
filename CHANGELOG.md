e7fd053495e7b9f1684868ffe74053720005a76a
Andrii Maliuta <quadr988@gmail.com>
2022-11-19 17:45:49 +0200

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for atlas-rest-haskell
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ app/Main.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Monad
+import Data.Foldable
+import Data.String
+import Data.Convertible
+import Data.List
+
+import Network.HTTP.Simple
+import Data.Aeson (Value)
+import Text.HTML.TagSoup
+import Text.StringLike(StringLike)
+import qualified Data.ByteString.Lazy.Char8 as L8
+
+import           Data.Aeson            (Value)
+import qualified Data.ByteString.Char8 as S8
+import qualified Data.Yaml             as Yaml
+
+
+-- main
+main :: IO ()
+main = do 
+  let bodyData = "{\"type\":\"page\", \"title\":\"new page\", \"space\": {\"key\":\"BBB\"},\"body\":{\"storage\":{\"value\":\"<p>This is <br/> page</p>\", \"representation\": \"storage\"}}}"
+
+  request' <- parseRequest "POST http://localhost:7202/rest/api/content"
+  
+  let request
+            = setRequestMethod "POST"
+            $ setRequestPath "/rest/api/content"
+            $ setRequestBodyLBS bodyData
+            $ setRequestSecure False
+            $ setRequestPort 7202
+            $ request'
+  
+  response <- httpJSON request
+
+--  S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
+--  resp <- httpLBS "http://localhost:7202/rest/api/content"
+  print "DOne!"
