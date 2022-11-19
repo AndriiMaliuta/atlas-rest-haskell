a4ef13fa27876dea9d059238ec65a15cb3fda958
Andrii Maliuta <quadr988@gmail.com>
2022-11-19 19:08:59 +0200

    init

--- app/Main.hs
+++ app/Main.hs
@@ -7,35 +7,49 @@ import Data.Foldable
 import Data.String
 import Data.Convertible
 import Data.List
-
 import Network.HTTP.Simple
-import Data.Aeson (Value)
+import Data.Aeson
 import Text.HTML.TagSoup
 import Text.StringLike(StringLike)
 import qualified Data.ByteString.Lazy.Char8 as L8
-
-import           Data.Aeson            (Value)
 import qualified Data.ByteString.Char8 as S8
 import qualified Data.Yaml             as Yaml
 
 
+data CreatePageReq = CreatePageReq String String
+
 -- main
 main :: IO ()
-main = do 
-  let bodyData = "{\"type\":\"page\", \"title\":\"new page\", \"space\": {\"key\":\"BBB\"},\"body\":{\"storage\":{\"value\":\"<p>This is <br/> page</p>\", \"representation\": \"storage\"}}}"
+main = do
+   let bodyData = "{\"type\":\"page\", \"title\":\"new page\", \"space\": {\"key\":\"BBB\"},\"body\":{\"storage\":{\"value\":\"<p>This is <br/> page</p>\", \"representation\": \"storage\"}}}"
+   request' <- parseRequest "POST http://localhost:7202/rest/api/content"
+   let request
+              = setRequestMethod "POST"
+              $ setRequestPath "/rest/api/content"
+              $ setRequestBodyLBS bodyData
+              $ setRequestSecure False
+              $ setRequestPort 7202
+              $ request'
 
-  request' <- parseRequest "POST http://localhost:7202/rest/api/content"
-  
-  let request
-            = setRequestMethod "POST"
-            $ setRequestPath "/rest/api/content"
-            $ setRequestBodyLBS bodyData
-            $ setRequestSecure False
-            $ setRequestPort 7202
-            $ request'
-  
-  response <- httpJSON request
+   response <- httpJSON request
+   S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
+   print "DOne!"
 
---  S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
---  resp <- httpLBS "http://localhost:7202/rest/api/content"
-  print "DOne!"
+
+postHttpSimple = do
+    let bodyData = "{\"type\":\"page\", \"title\":\"new page\", \"space\": {\"key\":\"BBB\"},\"body\":{\"storage\":{\"value\":\"<p>This is <br/> page</p>\", \"representation\": \"storage\"}}}"
+    request' <- parseRequest "POST http://localhost:7202/rest/api/content"
+    let request
+              = setRequestMethod "POST"
+              $ setRequestPath "/rest/api/content"
+              $ setRequestBodyLBS bodyData
+              $ setRequestSecure False
+              $ setRequestPort 7202
+              $ request'
+
+    response <- httpJSON request
+    S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
+  --  resp <- httpLBS "http://localhost:7202/rest/api/content"
+
+
+--postHttpDataBody = do
